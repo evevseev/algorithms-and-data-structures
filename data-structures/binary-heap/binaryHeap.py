bacb5d29df225d271c7e1160b81666fec56dcc70
Efmprof <evseev-2003@ya.ru>
2023-02-11 22:12:30 +0000

    Add binaryHeap py

---
+++ data-structures/binary-heap/binaryHeap.py
@@ -0,0 +1,61 @@
+class Heap:
+    def __init__(self):
+        self._data: list[int] = []
+
+    @staticmethod
+    def _get_left_child_index(parent: int) -> int:
+        return 2 * parent + 1
+
+    @staticmethod
+    def _get_right_child_index(parent: int) -> int:
+        return 2 * parent + 2
+
+    @staticmethod
+    def _get_parent_index(child: int) -> int:
+        return (child - 1) // 2
+
+    def _swap_elements(self, idx1: int, idx2: int):
+        temp_val = self._data[idx1]
+        self._data[idx1] = self._data[idx2]
+        self._data[idx2] = temp_val
+
+    def _heapify_up(self, idx: int):
+        while idx > 0 and self._data[self._get_parent_index(idx)] < self._data[idx]:
+            self._swap_elements(idx, self._get_parent_index(idx))
+            idx = self._get_parent_index(idx)
+
+    def _heapify_down(self, idx: int):
+        data_size = len(self._data)
+        while True:
+            l_idx = self._get_left_child_index(idx)
+            r_idx = self._get_right_child_index(idx)
+            largest_el_idx = idx
+
+            if l_idx < data_size and self._data[idx] < self._data[l_idx]:
+                largest_el_idx = l_idx
+
+            if r_idx < data_size and self._data[largest_el_idx] < self._data[r_idx]:
+                largest_el_idx = l_idx
+
+            if idx != largest_el_idx:
+                t_less = self._data[idx]
+                self._data[idx] = self._data[largest_el_idx]
+                self._data[largest_el_idx] = t_less
+
+                idx = largest_el_idx
+            else:
+                break
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def insert(self, val: int):
+        self._data.append(val)
+        self._heapify_up(len(self._data) - 1)
+
+    def extract(self) -> int:
+        res = self._data[0]
+        self._swap_elements(0, len(self._data) - 1)
+        self._data.pop()
+        self._heapify_down(0)
+        return res
